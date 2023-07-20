f5ea50c26a5aa894645ac1822c28d8e0ec0fc229
samahpowers@gmail.com <samahpowers@gmail.com>
2023-07-20 11:26:16 +0300

    5th commit on links

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const cardDivs = document.querySelectorAll(".card")
-
+const paragraph = document.querySelectorAll(".rsn")
+const contact =document.querySelector(".contact")
+const contactsPopUp = document.querySelector(".contacts-popup")
+document.addEventListener("click", (e)=>{
+  if(contact.contains(e.target)){
+contactsPopUp.style.display = "block"
+  } else{
+    contactsPopUp.style.display = "none"
+  }
+})
 
 // Loop through each div and apply a style to the even divs
 cardDivs.forEach((div, index) => {
@@ -9,3 +18,8 @@ cardDivs.forEach((div, index) => {
     // Add any other styles you want to apply to even divs
   }
 });
+paragraph.forEach((p, index)=>{
+  if(index % 2 === 1){
+    p.style.color = "rgb(111, 120, 255)"
+  }
+})
